65dd20a5a3143fa3fc25fec3ff3e7d43f1d645c1
Yana T <yana.work.related@gmail.com>
2024-06-17 15:15:30 -0700

    created item entity

--- receipt-processor/src/main/java/com/yana/receipt_processor/entities/Item.java
+++ receipt-processor/src/main/java/com/yana/receipt_processor/entities/Item.java
@@ -1,5 +1,20 @@
 package com.yana.receipt_processor.entities;
 import lombok.Data;
+@Data
 public class Item {
+    private String shortDescription;
+    private String price;
 
+    public Item(String shortDescription, String price) {
+        this.shortDescription = shortDescription;
+        this.price = price;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
 }
