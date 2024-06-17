4d901b05e11d5098960d792e629a8c35c7cf7c4f
Yana T <yana.work.related@gmail.com>
2024-06-17 15:06:18 -0700

    created receipt entity

---
+++ receipt-processor/src/main/java/com/yana/receipt_processor/entities/Item.java
@@ -0,0 +1,5 @@
+package com.yana.receipt_processor.entities;
+import lombok.Data;
+public class Item {
+
+}
---
+++ receipt-processor/src/main/java/com/yana/receipt_processor/entities/Receipt.java
@@ -0,0 +1,41 @@
+package com.yana.receipt_processor.entities;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Receipt {
+    private String retailer;
+    private String purchaseDate;
+    private String purchaseTime;
+    private List<Item> itemList;
+    private String totalPaid;
+
+    public Receipt(String retailer, String purchaseDate, String purchaseTime, List<Item> itemList, String totalPaid) {
+        this.retailer = retailer;
+        this.purchaseDate = purchaseDate;
+        this.purchaseTime = purchaseTime;
+        this.itemList = itemList;
+        this.totalPaid = totalPaid;
+    }
+
+    public String getRetailer() {
+        return retailer;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public String getPurchaseTime() {
+        return purchaseTime;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public String getTotalPaid() {
+        return totalPaid;
+    }
+}
